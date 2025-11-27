b5d6a85b2a1f30dc6e4d83f4e5bd427c4427c6cb
IMarcusDev <escobarmarcos511@gmail.com>
2025-11-27 08:51:43 -0500

    home_page table

--- lib/presentation/views/home_page.dart
+++ lib/presentation/views/home_page.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:pry_api_rest/presentation/viewmodels/product_viewmodel.dart';
 import 'package:pry_api_rest/presentation/widgets/base_button.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -12,26 +11,58 @@ class HomePage extends StatelessWidget {
     final vm = context.watch<ProductViewmodel>();
 
     return Scaffold(
-      appBar: AppBar(title: const Text("Productos")),
+      appBar: AppBar(title: const Text("Tabla de Productos")),
       body: vm.loading
           ? const Center(child: CircularProgressIndicator())
-          : ListView.builder(
-        itemCount: vm.products.length,
-        itemBuilder: (_, i) {
-          final p = vm.products[i];
-          return ListTile(
-            title: Text(p.name),
-            subtitle: Text("Precio: \$${p.price}"),
-          );
-        },
-      ),
-      floatingActionButton: BaseButton(
-        pressFunction: () {
+          : LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(minWidth: constraints.maxWidth),
+                      child: DataTable(
+                        columns: const [
+                          DataColumn(label: Text("Nombre")),
+                          DataColumn(label: Text("Precio")),
+                          DataColumn(label: Text("Acciones")),
+                        ],
+                        rows: vm.products.map((p) {
+                          return DataRow(
+                            cells: [
+                              DataCell(Text(p.name)),
 
-        },
-        backgroundColor: Colors.greenAccent,
-        child: Icon(Icons.add),
-      )
+                              DataCell(Text("\$${p.price}")),
+
+                              DataCell(
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    // Botón Editar
+                                    BaseButton(
+                                      pressFunction: (){}, 
+                                      backgroundColor: Colors.transparent,
+                                      child: Icon(Icons.edit, color: Colors.blue), 
+                                    ),
+                                    // Botón Eliminar
+                                    BaseButton(
+                                      pressFunction: (){}, 
+                                      backgroundColor: Colors.transparent,
+                                      child: Icon(Icons.delete, color: Colors.red), 
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
